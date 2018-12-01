52dc9c77ea5e0917d53aa08e3ae130750d36ed5f
Otto Seiskari <otto.seiskari@gmail.com>
2018-12-01 14:46:51 +0200

    Bugfix in half-space tracer

--- glsl/surfaces/half_space.glsl
+++ glsl/surfaces/half_space.glsl
@@ -1,6 +1,6 @@
 vec4 half_space_intersection(vec3 pos, vec3 ray, bool is_inside, vec3 plane_normal) {
     float s = -dot(ray, plane_normal);
-    if ((dot(pos, ray) > 0.0) != is_inside) return vec4(0,0,0,-1);
+    if ((dot(plane_normal, ray) > 0.0) != is_inside) return vec4(0,0,0,-1);
     float dist = -dot(pos, plane_normal) / dot(ray,plane_normal);
     return vec4(plane_normal, dist);
 }
