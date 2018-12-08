be6c30efa86595f126f14abd5dc02ded2ca91d2a
Otto Seiskari <otto.seiskari@gmail.com>
2018-12-08 11:40:13 +0200

    fixed g1

--- glsl/shading/cook_torrence.glsl
+++ glsl/shading/cook_torrence.glsl
@@ -3,10 +3,6 @@
 #include "util/math.glsl"
 #include "util/random_helpers.glsl"
 
-#ifndef MAX_WEIGHT
-#define MAX_WEIGHT 200.0
-#endif
-
 float ggx_D(vec3 n, vec3 m, float alpha) {
     float mn = dot(m, n);
     if (mn < 0.0) return 0.0;
@@ -22,11 +18,11 @@ float ggx_G1(vec3 v, vec3 m, vec3 n, float alpha) {
     float vm = dot(v, m);
     float vn = dot(v, n); // note: sign only depends on refraction vs reflection
     if (sign(vm) * sign(vn) < 0.0) return 0.0; // vm/vn < 0.0
-    float vm2 = vm*vm;
+    float vn2 = vn*vn;
     float a2 = alpha*alpha;
     // = 2 / (1 + sqrt(1 + alpha^2 tan^2(theta)))
     //return 2.0 / (1.0 + sqrt(max(1.0 + alpha * alpha * (1.0 - vm2) / vm2, 0.0)));
-    return 2.0 / (1.0 + sqrt(max(a2 / vm2 + 1.0 - a2, 0.0)));
+    return 2.0 / (1.0 + sqrt(max(a2 / vn2 + 1.0 - a2, 0.0)));
 }
 
 color_type fresnel_schlick_term(float cos_theta, color_type f0) {
@@ -81,8 +77,7 @@ bool ggx_sample_specular(float alpha, bool going_out, vec3 normal, inout vec3 ra
     vec3 o = ray - 2.0*dot(m, ray)*m;
 
     // f / p
-    float weight = min(dot(i, m) * ggx_G1(i, m, normal, alpha) * ggx_G1(o, m, normal, alpha) / (dot(i, normal) * dot(m, normal)), MAX_WEIGHT);
-    //float weight = dot(i, m) * ggx_G1(i, m, normal, alpha) * ggx_G1(o, m, normal, alpha) / dot(i, normal) * dot(m, normal);
+    float weight = dot(i, m) * ggx_G1(i, m, normal, alpha) * ggx_G1(o, m, normal, alpha) / (dot(i, normal) * dot(m, normal));
     color = vec3(1.0, 1.0, 1.0);
     //color = fresnel_schlick_term(-dot(ray, m), color);
     color *= weight;
@@ -100,7 +95,7 @@ float ggx_sampling_pdf(float alpha, bool going_out, vec3 normal, vec3 ray_in, ve
     float d = ggx_D(normal, m, alpha);
 
     float ch_vars = 1.0 / (4.0 * dot(ray_out, m));
-    return min(d * dot(m, normal) * ch_vars, MAX_WEIGHT);
+    return d * dot(m, normal) * ch_vars;
 }
 
 color_type ggx_specular_brdf(float alpha, bool going_out, vec3 normal, vec3 ray_in, vec3 ray_out) {
@@ -111,7 +106,7 @@ color_type ggx_specular_brdf(float alpha, bool going_out, vec3 normal, vec3 ray_
     float ch_vars = 1.0 / (4.0 * dot(ray_out, m));
     float weight = dot(-ray_in, m) * ggx_G1(-ray_in, m, normal, alpha) * ggx_G1(ray_out, m, normal, alpha) / (dot(-ray_in, normal) * dot(m, normal));
 
-    return color * min(d * dot(m, normal) * ch_vars * weight, MAX_WEIGHT);
+    return color * d * dot(m, normal) * ch_vars * weight;
 }
 
 float lambert_sampling_pdf(vec3 normal, vec3 ray_out) {
@@ -179,6 +174,7 @@ bool sample_ray(int material_id, bool going_out, vec3 normal, inout vec3 ray, ou
       ggx_sample_specular(1.0, going_out, normal, ray, col2, rng);
       color = col2 * get_diffuse(material_id);
       return true;*/
+
       return sample_diffuse_lambert(material_id, going_out, normal, ray, color, rng);
     }
 }
